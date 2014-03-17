58cf36c1d46430033830e0679646ef9a48d0100a
jesters320 <butler.brian.c@gmail.com>
2014-03-17 17:50:57 -0400

    adding winner to game show

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,10 @@ class Game < ActiveRecord::Base
 	belongs_to :away, class_name: 'Team', foreign_key: 'away_id'
 	
 	def find_winner
-		h = HomeScore.find_by(ones: self.home_score % 10)
-		a = AwayScore.find_by(ones: self.away_score % 10)
-		Block.find_by(home_score: h, away_score: a)
+		unless self.home_score.nil? || self.away_score.nil?
+			h = HomeScore.find_by(ones: self.home_score % 10)
+			a = AwayScore.find_by(ones: self.away_score % 10)
+			Block.find_by(home_score: h, away_score: a)
+		end
 	end
 end
