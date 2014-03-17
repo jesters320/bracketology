600c2e2a1023614642a372a2c7fd6f1087659148
jesters320 <butler.brian.c@gmail.com>
2014-03-17 12:40:53 -0400

    new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@
 HomeScore.delete_all
 AwayScore.delete_all
 Block.delete_all
+Team.delete_all
+Game.delete_all
 
 0.upto(9) do |h|
 	puts "Adding home: #{h}"
@@ -26,3 +28,25 @@ HomeScore.order(:display_order).each do |h|
 		Block.create(home_score: h, away_score: a)
 	end
 end
+
+1.upto(65) do |g|
+	Game.create(game_number: g)
+end
+
+# South
+Team.create(seed:2, name:'Kansas', abb: '')
+Team.create(seed:1, name:'Florida', abb: '')
+Team.create(seed:3, name:'Syracuse', abb: '')
+Team.create(seed:4, name:'UCLA', abb: '')
+Team.create(seed:5, name:'VCU', abb: '')
+Team.create(seed:6, name:'Ohio State', abb: '')
+Team.create(seed:7, name:'New Mexico', abb: '')
+Team.create(seed:8, name:'Colorado', abb: '')
+Team.create(seed:9, name:'Pittsburgh', abb: '')
+Team.create(seed:10, name:'Stanford', abb: '')
+Team.create(seed:11, name:'Dayton', abb: '')
+Team.create(seed:12, name:'Stephen F. Austin', abb: '')
+Team.create(seed:13, name:'Tulsa', abb: '')
+Team.create(seed:14, name:'Western Michigan', abb: '')
+Team.create(seed:15, name:'Eastern Kentucky', abb: '')
+Team.create(seed:16, name:'Play-in', abb: '')
