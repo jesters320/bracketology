adf1d98b25ff41ed1348f7836a91531b75e79700
jesters320 <butler.brian.c@gmail.com>
2014-03-17 11:13:13 -0400

    trying again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require analytics
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require analytics
 //= require_tree .
